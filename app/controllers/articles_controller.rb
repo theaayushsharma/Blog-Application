d6fa083292808f3594c843d27f9d7db4e56411ff
Aayush Sharma <theaayushsharma@Aayushs-MacBook-Air.local>
2017-07-12 14:19:14 +0530

    authentication and multiple models added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
 
-def index
+http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+ 
+  def index
     @articles = Article.all
   end
  
